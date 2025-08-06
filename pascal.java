5772b288a6def67d047c4aaa049c6e4fff1dfcc8
sakshamsaxena <122423845+saksham1815@users.noreply.github.com>
2025-08-06 20:13:08 +0530

    Create pascal.java

---
+++ pascal.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // Calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static int pascalTriangle(int r, int c) {
+        return (int) nCr(r - 1, c - 1);
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        // Generating Pascal's Triangle:
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                row.add((int) nCr(i, j));
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
